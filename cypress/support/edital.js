f564fad58d8fff2333237ee1187fc39db3a366b4
João Pedro Teixeira do Espirito Santo <txrajoao@gmail.com>
2025-07-01 15:14:05 -0400

    feat: adiciona nome de edital na env

--- cypress/support/edital.js
+++ cypress/support/edital.js
@@ -3,8 +3,8 @@ Cypress.Commands.add("preencheEdital",()=>{
     cy.get('[data-cy="nav-group-edital"]').click(); //Clica na aba Editais
     cy.get('[data-cy="nav-item-publicar-edital"]').click(); //Clica na opção Editais para acessar da página de Editais
     cy.get('[data-cy="add-publicar-edital"]').click(); //Clica no botão "Adicionar" para criação de um novo Edital
-    cy.get('[data-cy="nome"]').type(
-      'Grupo-01 E.S. 005/2025 joão-neves', //Edite essa linha para preencher o nome do Edital
+  cy.get('[data-cy="nome"]').type(
+    String(Cypress.env('nomeEdital') || 'Grupo-06 E.S. 005/2025'), //Edite essa linha para preencher o nome do Edital
       { delay: 0 },
     ); //Preenche o campo "Nome" do Edital
 });
