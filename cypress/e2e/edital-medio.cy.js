276c26245e31453e60552b12b7d81362682dd4db
Robert Coenga <rcoenga@inovvati.com.br>
2025-07-02 19:36:27 -0400

    refactor:Modularizado testes do completo e adicionado assert no medio

--- cypress/e2e/edital-medio.cy.js
+++ cypress/e2e/edital-medio.cy.js
@@ -3,76 +3,27 @@ import moment from 'moment';
 
 describe('Teste de E.M. (Edital Médio)',()=>{
       beforeEach(() => {
-    // Gancho em nível raíz
-    // executa antes de realizar cada teste(it)
     cy.login(
-      '/',// [URL do sistema]
-      Cypress.env("username"), // [E-mail do usuário]
-      Cypress.env("password"), // [Senha do usuário]
-    ); //Acessa a página de login usando as credenciais do usuário e senha.
+      '/',
+      Cypress.env("username"),
+      Cypress.env("password"),
+    );
   });
     it('Realiza login no sistema e cria um edital medio',()=>{
-      console.log(moment().format("DD/MM/YYYY HH:mm:ss"))
+        cy.intercept("POST",Cypress.env("baseUrl")+"/edital").as("criarEdital")
         cy.preencheEdital("Medio");
         cy.preencheCronograma();
         cy.preencheOrcamento();
-
         cy.get('[data-cy="menu-salvar"]').click(); //Clica no botão "Salvar" para salvar as informações do Edital
         cy.get('[data-cy="menu-finalizar"]').click(); //Clica no botão "Finalizar" para salvar e sair da área de adição do Edital
-    });
+        cy.wait("@criarEdital").then((req)=>{
+          expect(req.response.statusCode).to.equal(201);
+          cy.get('table[aria-labelledby="tableTitle"]').should("contain","Grupo-06 E.S. ");
+
+        })
+      });
 });
-// ## Atividade 1 - Realizar o Teste Edital Médio
-
-// 1. Crie um novo teste e2e: `edital-medio.cy.ts`.
-// 2. Neste teste resolva a solicitação abaixo:
-
-// ## Teste de E.M. (Edital Médio)
-
-// 1. Em Informações do Edital
-// - Em Identificação do Edital
-//     - Titulo do Edital
-//     > Utilizar: `[grupoalunos-numero] [E.M.] [código]/[ano] [nomealuno-sobrenomealuno]`
-//     >
-//     > Exemplo: Grupo-01 E.M. 005/2025 joão-neves
-//     >
-//     > [grupoalunos-numero]: Número do Grupo definido pelo Professor (01 a 99)
-//     >
-//     > [E.M.]: Edital Médio
-//     >
-//     > [código]: Código de 3 digitos (000 a 999)
-//     >
-//     > [ano]: Ano de criação do Edital
-//     >
-//     > [nomealuno-sobrenomealuno]: nome e sobrenome do acadêmico
-// - Em Restrições
-//     - Opções de Restrições [Checkbox]
-//     > Marcar a opção "Definir a duração do projeto em meses" e adicionar uma duração em quantidade de meses.
-//     >
-//     > Marcar a opção "Pesquisador pode submeter mais de uma proposta".
-// - Em Termo de Aceite
-//     - Adicionar um texto de Termo de Aceite
-// - Em Texto do Edital
-//     - Adicionar um Texto do Edital
-// - Em Abrangência
-//     - Adicionar mais de duas Abrangências (Escolha de preferência)
-// 2. Em Cronograma
-// - Em Período de Submissão
-//     - Adicionar a data inicial e final.
-//     > formato de data: DD/MM/YYYY hh:mm:ss
-//     >
-//     > A data final sempre deve ser posterior a data inicial.
-// 3. Em Orçamento
-// - Em Programa
-//     - Adicionar um Programa a este Edital
-//     > Selecionar um dos programas da `Caixa de Seleção`. 
-// 4. Em Perguntas
-// - Em Indicadores de Produção
-//     - Adicionar os três Indicadores de Produção
-// 5. Finalizar
-//     - Clicar no botão Salvar.
-//     - Clicar no botão Finalizar.
-
-// **Resultado esperado:**
+// ## Atividade 
 
 // Após a realização do Teste Cypress se espera que o Edital tenha sido criado com sucesso e apareça na tela de visualização dos Editais.
 
--- cypress/support/edital.js
+++ cypress/support/edital.js
@@ -12,10 +12,9 @@ Cypress.Commands.add("preencheEdital",(nivel)=>{
     cy.get('[data-cy="definirDuracaoProjetoEmMeses"]').check(); //Marca a opção "Definir Duração do Projeto em Meses"
     cy.get('[data-cy="duracaoProjetoEmMeses"]').type('6'); //Preenche o campo "Duração do Projeto em Meses com o valor 6"
     cy.get('[data-cy="pesquisadorSubmeterVariasPropostas"]').check(); //Marca a opção "Pesquisador pode submeter várias propostas"
-  switch(nivel){
-    case "Medio":
-      cy.intercept("/estado?withDeleted=false").as("estados")
-      cy.get('[data-cy="termo-de-aceite"]').click()//Clica na aba Termo de Aceite das Informações do Edital      
+    if(nivel != "Simples")
+    {
+      cy.intercept("/estado?withDeleted=false").as("estados")   
       // Termo de Aceite
       cy.get('[data-cy="termo-de-aceite"]').click(); //Clica na aba Termo de Aceite para seguir para a página de Termo de Aceite
       cy.get('[data-cy="termoDeAceite"]').click().realType('Aceito os termos de aceite')// Encontra a área editável do CKEditor, clica para ativar e depois digita.
@@ -23,22 +22,43 @@ Cypress.Commands.add("preencheEdital",(nivel)=>{
       // Texto do Edital
       cy.get('[data-cy="texto-do-edital"]').click(); //Clica na aba
       cy.get('[data-cy="texto"]').realType('Texto completo do Edital Medio.'); //Preenche o campo "Texto do Edital" com o texto "Texto completo do Edital Médio."
+      if(nivel == "Medio"){
+        // Abrangência
+        cy.get('[data-cy="abrangencia"]').click(); 
+        cy.wait("@estados").then((e)=>{
+          for(let i=0;i<3;i++){
+            cy.contains(e.response.body.data[Math.floor(Math.random() * e.response.body.count)].nome).click();
+          }
+        });
+      }
+      else if(nivel == "Completo"){
+        // Abrangência
+        cy.get('[data-cy="abrangencia"]').click(); //Clica na aba Abrangência para seguir para a página de Abrangência
+        cy.get('[data-cy="estado-todos"]').click(); //Clica no botão "todos" para criar uma nova Abrangência
+        // Informacoes complementares
+        cy.get('[data-cy="informacoes-complementares"]').click(); //Clica na aba informacoes
 
-      // Abrangência
-      cy.get('[data-cy="abrangencia"]').click(); 
-      cy.wait("@estados").then((e)=>{
-        for(let i=0;i<3;i++){
-          cy.contains(e.response.body.data[Math.floor(Math.random() * e.response.body.count)].nome).click();
-        }
-        console.log(e.response.body.data);
-      });
+        cy.get('[data-cy="perguntaInfoId"]').click(); //Clica no campo de seleção de Pergunta
+        cy.get('[data-cy="ocupacao-da-equi"]').click(); //Seleciona a Pergunta "Ocupação da Equipe" da lista de Perguntas
+        cy.get('[data-cy="informacaoComplementarPergunta-adicionar"]').click(); //Clica no botão "Adicionar Pergunta" para criar uma nova Pergunta
 
-    break;
-    case "Completo":
-    break;
-    default:
-    break;
-  }
+        cy.get('[data-cy="perguntaInfoId"]').click(); //Clica no campo de seleção de Pergunta
+        cy.get('[data-cy="data-de-realizac"]').click(); //Clica no campo de seleção de Data de Realização
+        cy.get('[data-cy="informacaoComplementarPergunta-adicionar"]').click(); //botao add
+
+        cy.get('[data-cy="perguntaInfoId"]').click(); //Clica no campo de seleção de Pergunta
+        cy.get('[data-cy="porte-da-empresa"]').click(); //Seleciona a Pergunta "Porte da Empresa" da lista de Perguntas
+        cy.get('[data-cy="informacaoComplementarPergunta-adicionar"]').click(); //botao add
+
+        cy.get('[data-cy="perguntaInfoId"]').click(); //Clica no campo de seleção de Pergunta
+        cy.get('[data-cy="areas-tematicas"]').click(); //Seleciona a Pergunta "Áreas Temáticas" da lista de Perguntas
+        cy.get('[data-cy="informacaoComplementarPergunta-adicionar"]').click(); //botao add
+
+        cy.get('[data-cy="perguntaInfoId"]').click(); //Clica no campo de seleção de Pergunta
+        cy.get('[data-cy="objetivos-de-des"]').click(); //Seleciona a Pergunta "Objetivos de Desenvolvimento Sustentável" da lista de Perguntas
+        cy.get('[data-cy="informacaoComplementarPergunta-adicionar"]').click(); //botao add
+      }
+    }
  
 });
 Cypress.Commands.add("preencheCronograma",()=>{
@@ -49,12 +69,83 @@ Cypress.Commands.add("preencheCronograma",()=>{
     cy.get('[data-cy="chamadaUnsaved.termino"]').type(moment().add(1,'year').format("DD/MM/YYYY HH:mm:ss"));//Preenche o campo "Término" do Período de Submissão com a data do dia de hoje + 1 ano
     cy.get('[data-cy="chamada-confirmar"]').click(); //Clica no botão "Salvar" para salvar as informações do Período de Submissão
   });
-Cypress.Commands.add("preencheOrcamento",()=>{
+Cypress.Commands.add("preencheOrcamento",(nivel)=>{
     // Orçamento
     cy.get('[data-cy="orcamento"]').click(); //Clica na aba Orçamento para exibir as opções de Orçamento
     cy.get('[data-cy="programa"]').click(); //Clica em Programa para seguir para a página de Programa
     cy.get('[data-cy="programaId"]').click(); //Clica no campo de seleção de Programa
     cy.get('[data-cy-index="programaId-item-0"]').click(); //Seleciona o primeiro Programa da lista de Programas
+     if(nivel == "Completo"){
+        // Rubricas
+        cy.get('[data-cy="rubricas"]').click(); //Clica na aba Rubricas para seguir para a página de Rubricas
+        //Rubrica1
+        cy.get('[data-cy="add-button"]').click(); //Clica no botão "Adicionar" para criar uma nova Rubrica
+        cy.get('[data-cy="editalRubricaUnsaved.tipoEditalRubrica"]').click(); //Clica no campo de seleção de Tipo de Rubrica
+        cy.get('[data-cy="diarias"]').click(); //Seleciona o Tipo de Rubrica "Diárias" da lista de Tipos de Rubricas
+        cy.get('[data-cy="editalRubricaUnsaved.naturezaDespesaId"]').click(); //Clica no campo de seleção de Natureza da Despesa
+        cy.get('[data-cy="custeio"]').click(); //Seleciona o Tipo de Rubrica "Custeio" da lista de Tipos de Rubricas
+        cy.get('[data-cy="editalRubrica-confirmar"]').click(); //Clica no botão "Salvar" para salvar as informações da Rubrica
+        //Rubrica2
+        cy.get('[data-cy="add-button"]').click(); //Clica no botão "Adicionar" para criar uma nova Rubrica
+        cy.get('[data-cy="editalRubricaUnsaved.tipoEditalRubrica"]').click(); //Clica no campo de seleção de Tipo de Rubrica
+        cy.get('[data-cy="hospedagem-e-ali"]').click(); //Seleciona o Tipo de Rubrica "Diárias" da lista de Tipos de Rubricas
+        cy.get('[data-cy="editalRubricaUnsaved.naturezaDespesaId"]').click(); //Clica no campo de seleção de Natureza da Despesa
+        cy.get('[data-cy="capital"]').click(); //Seleciona o Tipo de Rubrica "Custeio" da lista de Tipos de Rubricas
+        cy.get('[data-cy="editalRubrica-confirmar"]').click(); //Clica no botão "Salvar" para salvar as informações da Rubrica
+        //Rubrica3
+        cy.get('[data-cy="add-button"]').click(); //Clica no botão "Adicionar" para criar uma nova Rubrica
+        cy.get('[data-cy="editalRubricaUnsaved.tipoEditalRubrica"]').click(); //Clica no campo de seleção de Tipo de Rubrica
+        cy.get('[data-cy="material-de-cons"]').click(); //Seleciona o Tipo de Rubrica "Diárias" da lista de Tipos de Rubricas
+        cy.get('[data-cy="editalRubricaUnsaved.naturezaDespesaId"]').click(); //Clica no campo de seleção de Natureza da Despesa
+        cy.get('[data-cy="auxilio-a-pesqui"]').click(); //Seleciona o Tipo de Rubrica "Custeio" da lista de Tipos de Rubricas
+        cy.get('[data-cy="editalRubrica-confirmar"]').click(); //Clica no botão "Salvar" para salvar as informações da Rubrica
+        //Rubrica4
+        cy.get('[data-cy="add-button"]').click(); //Clica no botão "Adicionar" para criar uma nova Rubrica
+        cy.get('[data-cy="editalRubricaUnsaved.tipoEditalRubrica"]').click(); //Clica no campo de seleção de Tipo de Rubrica
+        cy.get('[data-cy="material-permane"]').click(); //Seleciona o Tipo de Rubrica "Diárias" da lista de Tipos de Rubricas
+        cy.get('[data-cy="editalRubricaUnsaved.naturezaDespesaId"]').click(); //Clica no campo de seleção de Natureza da Despesa
+        cy.get('[data-cy="capital"]').click(); //Seleciona o Tipo de Rubrica "Custeio" da lista de Tipos de Rubricas
+        cy.get('[data-cy="editalRubrica-confirmar"]').click(); //Clica no botão "Salvar" para salvar as informações da Rubrica
+
+
+        // Faixas de Financiamento
+        cy.get('[data-cy="faixas-de-financiamento"]').click(); //Clica na aba Faixas de Financiamento para seguir para a página de Faixas de Financiamento
+
+        cy.get('[data-cy="add-button"]').click(); //Clica no botão "Adicionar" para criar uma nova Faixa de Financiamento
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMinimo"]').type('0001'); // Valor Mínimo invertido
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMaximo"]').type('0005'); // Valor Máximo invertido
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.nome"]').type('Faixa 1');
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.observacao"]').type('Descrição da Faixa 1');
+        cy.get('[data-cy="faixaFinanciamento-confirmar"]').click();
+
+        cy.get('[data-cy="add-button"]').click(); // Segunda faixa
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMinimo"]').type('1005'); // invertido
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMaximo"]').type('00000000000001'); // invertido
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.nome"]').type('Faixa 2');
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.observacao"]').type('Descrição da Faixa 2');
+        cy.get('[data-cy="faixaFinanciamento-confirmar"]').click();
+
+        cy.get('[data-cy="add-button"]').click(); // Terceira faixa
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMinimo"]').type('011210'); // invertido
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMaximo"]').type('00000000002'); // invertido
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.nome"]').type('Faixa 3');
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.observacao"]').type('Descrição da Faixa 3');
+        cy.get('[data-cy="faixaFinanciamento-confirmar"]').click();
+
+        cy.get('[data-cy="add-button"]').click(); // Quarta faixa
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMinimo"]').type('02222'); // invertido
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMaximo"]').type('00555'); // invertido
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.nome"]').type('Faixa 4');
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.observacao"]').type('Descrição da Faixa 4');
+        cy.get('[data-cy="faixaFinanciamento-confirmar"]').click();
+
+        cy.get('[data-cy="add-button"]').click(); // Quinta faixa
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMinimo"]').type('105'); // invertido
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMaximo"]').type('100000001'); // invertido
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.nome"]').type('Faixa 5');
+        cy.get('[data-cy="faixaFinanciamentoUnsaved.observacao"]').type('Descrição da Faixa 5');
+        cy.get('[data-cy="faixaFinanciamento-confirmar"]').click();
+      }
 });
 Cypress.Commands.add("preenchePergunta", ()=>{
       // Perguntas
@@ -75,10 +166,5 @@ Cypress.Commands.add("preenchePergunta", ()=>{
       cy.get('[data-cy="indicadorProducaoUnsaved.id"]').click(); //Clica no campo de seleção de Indicador de Produção
       cy.get('[data-cy="producao-tecnica"]').click(); // Seleciona o terceiro da lista
       cy.get('[data-cy="indicadorProducao-confirmar"]').click();
-});
-Cypress.Commands.add("selecionaPrograma",()=>{
-    cy.get('[data-cy="orcamento"]').click(); //Clica na aba Orçamento para exibir as opções de Orçamento
-    cy.get('[data-cy="programa"]').click(); //Clica em Programa para seguir para a página de Programa
-    cy.get('[data-cy="programaId"]').click(); //Clica no campo de seleção de Programa
-    cy.get('[data-cy-index="programaId-item-0"]').click(); //Seleciona o primeiro Programa da lista de Programas
+     
 });
